5da63671ba847b25b18080744eec68ac15ad2f75
iteslarasu <iteslarasu@deloittece.com>
2023-07-24 15:26:03 +0300

    CNP exercise

--- src/main/java/javaExercises/ArrayExercises.java
+++ src/main/java/javaExercises/ArrayExercises.java
@@ -2,28 +2,27 @@ package javaExercises;
 
 public class ArrayExercises {
 
-    int[] arrControl = {2, 7, 9, 1, 4, 6, 3, 5, 8, 2, 7, 9};
-    int[] arrCNP = new int[13];
-
-    public int[] verifyCNP(int[] arrCNP) {
+    final int[] arrControl = {2, 7, 9, 1, 4, 6, 3, 5, 8, 2, 7, 9};
+    public String verifyCNP(int[] arrCNP) {
         int i;
         int sum = 0;
-        int sumRemainder = 0;
+        int sumRemainder;
         int cfControl = 0;
         int[] multiplyArr = new int[arrCNP.length];
         for (i = 0; i < arrControl.length; i++) {
             multiplyArr[i] = arrCNP[i] * arrControl[i];
-            sum = sum + multiplyArr[i];
+            sum += multiplyArr[i];
         }
         sumRemainder = sum % 11;
         if (sumRemainder < 10) {
             cfControl = sumRemainder;
-            arrCNP[12] = cfControl;
-        }if(sumRemainder == 10){
-            cfControl = 1;
-            arrCNP[13] = cfControl;
         }
-        return arrCNP;
-
+        if (sumRemainder == 10) {
+            cfControl = 1;
+        }
+        if (cfControl == 0)
+            return "CNP is correct";
+        else
+            return "CNP is incorrect";
     }
 }
--- src/main/java/javaExercises/MainClass.java
+++ src/main/java/javaExercises/MainClass.java
@@ -1,10 +1,10 @@
 package javaExercises;
 
 public class MainClass {
+
     public static void main(String[] args) {
-
-
         ArrayExercises myArr = new ArrayExercises();
-        myArr.verifyCNP(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3});
+        int [] cnp = {2,9,9,0,2,1,9,4,6,9,0,0,0};
+        System.out.println(myArr.verifyCNP(cnp));
     }
 }
