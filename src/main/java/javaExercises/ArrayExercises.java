37c57b81189c4f24f658c225b91c3e69f2d2278c
iteslarasu <iteslarasu@deloittece.com>
2023-08-03 18:05:47 +0300

    Fourth part of array exercises

--- src/main/java/javaExercises/ArrayExercises.java
+++ src/main/java/javaExercises/ArrayExercises.java
@@ -1,5 +1,4 @@
 package javaExercises;
-
 public class ArrayExercises {
 
     final int[] arrControl = {2, 7, 9, 1, 4, 6, 3, 5, 8, 2, 7, 9};
@@ -54,6 +53,7 @@ public class ArrayExercises {
         numsRotated[2] = nums[0];
         return numsRotated;
     }
+
     public int[] reverse3(int[] arr) {
         int[] reverseArr = new int[3];
         reverseArr[0] = arr[2];
@@ -61,46 +61,173 @@ public class ArrayExercises {
         reverseArr[2] = arr[0];
         return reverseArr;
     }
+
     public int[] maxEnd3(int[] arr) {
         int max = arr[0];
-        if(arr[2] > arr[0])
+        if (arr[2] > arr[0])
             max = arr[2];
-        arr[0]=arr[1]=arr[2]=max;
+        arr[0] = arr[1] = arr[2] = max;
         return arr;
     }
+
     public int sum2(int[] arr) {
         int sum;
-        if(arr.length < 1)
+        if (arr.length < 1)
             sum = 0;
-        else if(arr.length == 1)
+        else if (arr.length == 1)
             sum = arr[0];
         else
             sum = arr[0] + arr[1];
         return sum;
     }
+
     public int[] middleWay(int[] a, int[] b) {
         int[] newArr = new int[2];
         newArr[0] = a[1];
         newArr[1] = b[1];
         return newArr;
     }
+
     public int[] makeEnds(int[] arr) {
         int[] newArr = new int[2];
         newArr[0] = arr[0];
-        newArr[1] = arr[arr.length-1];
+        newArr[1] = arr[arr.length - 1];
         return newArr;
     }
+
     public boolean has23(int[] arr) {
         return (arr[0] == 2 || arr[0] == 3 || arr[1] == 3 || arr[1] == 2);
     }
+
     public boolean no23(int[] arr) {
         return ((arr[0] != 2 && arr[0] != 3) && (arr[1] != 3 && arr[1] != 2));
     }
-    
 
+    public int[] makeLast(int[] arr) {
+        int[] newArr = new int[2 * arr.length];
+        newArr[newArr.length - 1] = arr[arr.length - 1];
+        return newArr;
+    }
 
+    public boolean double23(int[] arr) {
+        int count2 = 0;
+        int count3 = 0;
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if (arr[i] == 2)
+                count2++;
+            if (arr[i] == 3)
+                count3++;
+        }
+        return count2 == 2 || count3 == 2;
+    }
 
+    public int[] fix23(int[] arr) {
+        for (int i = 0; i <= arr.length - 1; i++) {
+            if (arr[i] == 2 && arr[i + 1] == 3) {
+                arr[i + 1] = 0;
+                break;
+            }
+        }
+        return arr;
+    }
 
+    public int start1(int[] a, int[] b) {
+        int count = 0;
+        if (a.length > 0 && a[0] == 1)
+            count++;
+        if (b.length > 0 && b[0] == 1)
+            count++;
+        return count;
+    }
 
+    public int[] biggerTwo(int[] a, int[] b) {
+        int sumA = 0;
+        int sumB = 0;
+        for (int i = 0; i <= a.length - 1; i++)
+            sumA = sumA + a[i];
+        for (int j = 0; j <= a.length - 1; j++)
+            sumB = sumB + b[j];
+        if (sumB > sumA)
+            return b;
+        else
+            return a;
+    }
+
+    public int[] makeMiddle(int[] a) {
+        int[] newArr = new int[2];
+        newArr[0] = a[a.length / 2 - 1];
+        newArr[1] = a[a.length / 2];
+        return newArr;
+    }
+
+    public int[] plusTwo(int[] a, int[] b) {
+        int[] newArr = new int[4];
+        newArr[0] = a[0];
+        newArr[1] = a[1];
+        newArr[2] = b[0];
+        newArr[3] = b[1];
+        return newArr;
+    }
+
+    public int[] swapEnds(int[] arr) {
+        int aux;
+        aux = arr[0];
+        arr[0] = arr[arr.length - 1];
+        arr[arr.length - 1] = aux;
+        return arr;
+    }
+
+    public int[] midThree(int[] arr) {
+        int[] newArr = new int[3];
+        int i;
+        for (i = 0; i <= arr.length; i++) {
+            if (i == (arr.length / 2) - 1) {
+                newArr[0] = arr[i];
+            }
+            if (i == (arr.length / 2)) {
+                newArr[1] = arr[i];
+            }
+            if (i == (arr.length / 2) + 1) {
+                newArr[2] = arr[i];
+            }
+        }
+        return newArr;
+    }
+
+    public int maxTriple(int[] arr) {
+        int max = arr[0];
+        if (arr[(arr.length / 2)] > max)
+            max = arr[(arr.length / 2)];
+        if (arr[(arr.length - 1)] > max)
+            max = arr[(arr.length - 1)];
+        return max;
+    }
+
+    public int[] frontPiece(int[] a) {
+        int[] newArr = new int[2];
+        if (a.length > 2) {
+            for (int i = 0; i <= 1; i++) {
+                newArr[i] = a[i];
+            }
+            return newArr;
+        } else
+            return a;
+    }
+    public boolean unlucky1(int[] a) {
+        int unlucky = 0;
+        if(a.length >= 2)
+            if((a[0] == 1 && a[1] == 3)||(a[1] == 1 && a[2] == 3)||(a[a.length-2] == 1 && a[a.length-1] == 3))
+                unlucky++;
+        return unlucky == 1;
+    }
 
 }
+
+
+
+
+
+
+
+
+
--- src/main/java/javaExercises/Main.java
+++ src/main/java/javaExercises/Main.java
@@ -79,16 +79,16 @@ public class Main {
         /*Given an array of ints, return the sum of the first 2 elements in the array.
         If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.*/
         //sum2([1, 2, 3]) → 3
-        System.out.println(myArr.sum2(new int[]{1,2,3}));
+        System.out.println(myArr.sum2(new int[]{1, 2, 3}));
         //sum2([1, 1]) → 2
-        System.out.println(myArr.sum2(new int[]{1,1}));
+        System.out.println(myArr.sum2(new int[]{1, 1}));
         //sum2([1, 1, 1, 1]) → 2
-        System.out.println(myArr.sum2(new int[]{1,1,1,1}));
+        System.out.println(myArr.sum2(new int[]{1, 1, 1, 1}));
         //sum2([1]) → 1
         System.out.println(myArr.sum2(new int[]{1}));
         //sum2([]) → 0
         System.out.println(myArr.sum2(new int[]{}));
-        
+
         //Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
         //middleWay([1, 2, 3], [4, 5, 6]) → [2, 5]
         System.out.println(Arrays.toString(myArr.middleWay(new int[]{1, 2, 3}, new int[]{4, 5, 6})));
@@ -99,19 +99,162 @@ public class Main {
 
         //Given an int array length 2, return true if it contains a 2 or a 3.
         //has23([2, 5]) → true
-        System.out.println(myArr.has23(new int[]{2,5}));
+        System.out.println(myArr.has23(new int[]{2, 5}));
         //has23([4, 3]) → true
-        System.out.println(myArr.has23(new int[]{4,3}));
+        System.out.println(myArr.has23(new int[]{4, 3}));
         //has23([4, 5]) → false
-        System.out.println(myArr.has23(new int[]{4,5}));
+        System.out.println(myArr.has23(new int[]{4, 5}));
 
         //Given an int array length 2, return true if it does not contain a 2 or 3.
         //no23([4, 5]) → true
-        System.out.println(myArr.no23(new int[]{4,5}));
+        System.out.println(myArr.no23(new int[]{4, 5}));
         //no23([4, 2]) → false
-        System.out.println(myArr.no23(new int[]{4,2}));
+        System.out.println(myArr.no23(new int[]{4, 2}));
         //no23([3, 5]) → false
-        System.out.println(myArr.no23(new int[]{3,5}));
+        System.out.println(myArr.no23(new int[]{3, 5}));
 
+        //Given an int array, return a new array with double the length where
+        //its last element is the same as the original array, and all the other elements are 0.
+        //The original array will be length 1 or more.
+        //makeLast([4, 5, 6]) → [0, 0, 0, 0, 0, 6]
+        System.out.println(Arrays.toString(myArr.makeLast(new int[]{4, 5, 6})));
+        //makeLast([1, 2]) → [0, 0, 0, 2]
+        System.out.println(Arrays.toString(myArr.makeLast(new int[]{1, 2})));
+        //makeLast([3]) → [0, 3]
+        System.out.println(Arrays.toString(myArr.makeLast(new int[]{3})));
+        //makeLast([0]) → [0, 0]
+        System.out.println(Arrays.toString(myArr.makeLast(new int[]{0})));
+        //makeLast([7, 7, 7]) → [0, 0, 0, 0, 0, 7]
+        System.out.println(Arrays.toString(myArr.makeLast(new int[]{7, 7, 7})));
+
+        /*Given an int array, return true if the array contains 2 twice, or 3 twice. The array will be length 0, 1, or 2.*/
+        //double23([2, 2]) → true
+        System.out.println(myArr.double23(new int[]{2, 2}));
+        //double23([3, 3]) → true
+        System.out.println(myArr.double23(new int[]{3, 3}));
+        //double23([2, 3]) → false
+        System.out.println(myArr.double23(new int[]{2, 3}));
+        //double23([3, 2]) → false
+        System.out.println(myArr.double23(new int[]{3, 2}));
+        //double23([4, 5]) → false
+        System.out.println(myArr.double23(new int[]{4, 5}));
+
+        //Given an int array length 3, if there is a 2 in the array immediately followed by a 3, set the 3 element to 0.
+        //Return the changed array.
+        //fix23([1, 2, 3]) → [1, 2, 0]
+        System.out.println(Arrays.toString(myArr.fix23(new int[]{1, 2, 3})));
+        //fix23([2, 3, 5]) → [2, 0, 5]
+        System.out.println(Arrays.toString(myArr.fix23(new int[]{2, 3, 5})));
+        //fix23([1, 2, 1]) → [1, 2, 1]
+        System.out.println(Arrays.toString(myArr.fix23(new int[]{1, 2, 1})));
+        //fix23([3, 2, 1]) → [3, 2, 1]
+        System.out.println(Arrays.toString(myArr.fix23(new int[]{3, 2, 1})));
+
+        //Start with 2 int arrays, a and b, of any length.
+        //Return how many of the arrays have 1 as their first element.
+        //start1([1, 2, 3], [1, 3]) → 2
+        System.out.println(myArr.start1(new int[]{1, 2, 3}, new int[]{1, 3}));
+        //start1([7, 2, 3], [1]) → 1
+        System.out.println(myArr.start1(new int[]{7, 2, 3}, new int[]{1}));
+        //start1([1, 2], []) → 1
+        System.out.println(myArr.start1(new int[]{1, 2}, new int[]{}));
+        //start1([], [1, 2]) → 1
+        System.out.println(myArr.start1(new int[]{}, new int[]{1, 3}));
+        //start1([7], []) → 0
+        System.out.println(myArr.start1(new int[]{7}, new int[]{}));
+        //start1([1], [1]) → 2
+        System.out.println(myArr.start1(new int[]{1}, new int[]{1}));
+        //start1([], []) → 0
+        System.out.println(myArr.start1(new int[]{}, new int[]{}));
+
+        //Start with 2 int arrays, a and b, each length 2. Consider the sum of the values in each array.
+        //Return the array which has the largest sum. In event of a tie, return a.
+        //biggerTwo([1, 2], [3, 4]) → [3, 4]
+        System.out.println(Arrays.toString(myArr.biggerTwo(new int[]{1, 2}, new int[]{3, 4})));
+        //biggerTwo([3, 4], [1, 2]) → [3, 4]
+        System.out.println(Arrays.toString(myArr.biggerTwo(new int[]{3, 4}, new int[]{1, 2})));
+        //biggerTwo([1, 1], [1, 2]) → [1, 2]
+        System.out.println(Arrays.toString(myArr.biggerTwo(new int[]{1, 1}, new int[]{1, 2})));
+        //biggerTwo([2, 1], [1, 1]) → [2, 1]
+        System.out.println(Arrays.toString(myArr.biggerTwo(new int[]{2, 1}, new int[]{1, 1})));
+        //biggerTwo([2, 2], [1, 3]) → [2, 2]
+        System.out.println(Arrays.toString(myArr.biggerTwo(new int[]{2, 2}, new int[]{1, 3})));
+        //biggerTwo([1, 3], [1, 3]) → [1, 3]
+        System.out.println(Arrays.toString(myArr.biggerTwo(new int[]{1, 3}, new int[]{1, 3})));
+
+        /*Given an array of ints of even length, return a new array length 2 containing the middle two elements from the original array.
+         The original array will be length 2 or more.*/
+        //makeMiddle([1, 2, 3, 4]) → [2, 3]
+        System.out.println(Arrays.toString(myArr.makeMiddle(new int[]{1, 2, 3, 4})));
+        //makeMiddle([7, 1, 2, 3, 4, 9]) → [2, 3]
+        System.out.println(Arrays.toString(myArr.makeMiddle(new int[]{7, 1, 2, 3, 4, 9})));
+        //makeMiddle([1, 2]) → [1, 2]
+        System.out.println(Arrays.toString(myArr.makeMiddle(new int[]{1, 2})));
+
+        //Given 2 int arrays, each length 2, return a new array length 4 containing all their elements.
+        //plusTwo([1, 2], [3, 4]) → [1, 2, 3, 4]
+        System.out.println(Arrays.toString(myArr.plusTwo(new int[]{1, 2}, new int[]{3, 4})));
+        //plusTwo([4, 4], [2, 2]) → [4, 4, 2, 2]
+        System.out.println(Arrays.toString(myArr.plusTwo(new int[]{4, 4}, new int[]{2, 2})));
+        //plusTwo([9, 2], [3, 4]) → [9, 2, 3, 4]
+        System.out.println(Arrays.toString(myArr.plusTwo(new int[]{9, 2}, new int[]{3, 4})));
+
+        //Given an array of ints, swap the first and last elements in the array. Return the modified array. The array length will be at least 1.
+        //swapEnds([1, 2, 3, 4]) → [4, 2, 3, 1]
+        System.out.println(Arrays.toString(myArr.swapEnds(new int[]{1, 2, 3, 4})));
+        //swapEnds([1, 2, 3]) → [3, 2, 1]
+        System.out.println(Arrays.toString(myArr.swapEnds(new int[]{1, 2, 3})));
+        //swapEnds([8, 6, 7, 9, 5]) → [5, 6, 7, 9, 8]
+        System.out.println(Arrays.toString(myArr.swapEnds(new int[]{8, 6, 7, 9, 5})));
+        //swapEnds([1]) → [1]
+        System.out.println(Arrays.toString(myArr.swapEnds(new int[]{1})));
+
+        //Given an array of ints of odd length, return a new array length 3 containing the elements from the middle of the array.
+        //The array length will be at least 3.
+        //midThree([1, 2, 3, 4, 5]) → [2, 3, 4]
+        System.out.println(Arrays.toString(myArr.midThree(new int[]{1, 2, 3, 4, 5})));
+        //midThree([8, 6, 7, 5, 3, 0, 9]) → [7, 5, 3]
+        System.out.println(Arrays.toString(myArr.midThree(new int[]{8, 6, 7, 5, 3, 0, 9})));
+        //midThree([1, 2, 3]) → [1, 2, 3]
+        System.out.println(Arrays.toString(myArr.midThree(new int[]{1, 2, 3})));
+
+        /*Given an array of ints of odd length, look at the first, last, and middle values in the array and return the largest.
+        The array length will be a least 1.*/
+        //maxTriple([1, 2, 3]) → 3
+        System.out.println(myArr.maxTriple(new int[]{1, 2, 3}));
+        //maxTriple([1, 5, 3]) → 5
+        System.out.println(myArr.maxTriple(new int[]{1, 5, 3}));
+        //maxTriple([5, 2, 3]) → 5
+        System.out.println(myArr.maxTriple(new int[]{5, 2, 3}));
+        //maxTriple([5, 1, 7, 3, 7, 8, 1]) → 5
+        System.out.println(myArr.maxTriple(new int[]{5, 1, 7, 3, 7, 8, 1}));
+
+        /*Given an int array of any length, return a new array of its first 2 elements.
+        If the array is smaller than length 2, use whatever elements are present.*/
+        //frontPiece([1, 2, 3]) → [1, 2]
+        System.out.println(Arrays.toString(myArr.frontPiece(new int[]{1, 2, 3})));
+        //frontPiece([1, 2]) → [1, 2]
+        System.out.println(Arrays.toString(myArr.frontPiece(new int[]{1, 2})));
+        //frontPiece([1]) → [1]
+        System.out.println(Arrays.toString(myArr.frontPiece(new int[]{1})));
+        //frontPiece([3, 1, 4, 1, 5]) → [3, 1]
+        System.out.println(Arrays.toString(myArr.frontPiece(new int[]{3, 1, 4, 1, 5})));
+
+        /*We'll say that a 1 immediately followed by a 3 in an array is an "unlucky" 1.
+        Return true if the given array contains an unlucky 1 in the first 2 or last 2 positions in the array.*/
+        //unlucky1([1, 3, 4, 5]) → true
+        System.out.println(myArr.unlucky1(new int[]{1,3,4,5}));
+        //unlucky1([2, 1, 3, 4, 5]) → true
+        System.out.println(myArr.unlucky1(new int[]{2,1,3,4,5}));
+        //unlucky1([1, 1, 1]) → false
+        System.out.println(myArr.unlucky1(new int[]{1,1,1}));
+        //unlucky1([1, 3]) → true
+        System.out.println(myArr.unlucky1(new int[]{1,3}));
+        //unlucky1([1, 4]) → false
+        System.out.println(myArr.unlucky1(new int[]{1,4}));
+        //unlucky1([1]) → false
+        System.out.println(myArr.unlucky1(new int[]{1}));
+        //unlucky1([]) → false
+        System.out.println(myArr.unlucky1(new int[]{}));
     }
 }
